1aca79a2d44dd2d9ccd3d70a4640d6ebedb6442f
kennp174 <PhoebeKennedy@Phoebes-Air-2.wireless.newschool.edu>
2016-03-08 18:22:59 -0500

    classwork

--- sketch.js
+++ sketch.js
@@ -7,27 +7,50 @@
 //================================
 
 //STEP 1: Declare global variables for your images and upload button
+var uploadButton;
+var blurImage,girl,love;
+var img;
 
 function preload(){
 	//STEP 2:
 	// Load your image with loadImage("path/to/image.jpg")
+	//bubbles = loadImage('images/bubbles.jpg');
+	blurImage = loadImage('images/bubbles.jpg');
+	girl = loadImage('images/girl.png');
+	love = loadImage('images/love.png');
 	//and save it to your global variable:
-
-
 }
 
 function setup() {
-
 	//STEP 3
 	//add a file upload button with CreateFileInput(_____);
-
+	uploadButton =createFileInput(imageUploaded);
 	//STEP 4:
   //set up your canvas with createCanvas(__,__);
+	createCanvas(blurImage.width,blurImage.height);
+	blurImage.filter(BLUR,5);
+}
+function imageUploaded(file){
+	//img= loadImage(file.data, drawMyImage);
+	img= loadImage(file.data);
+}
 
+function drawMyImage(){
+		image(img);
 }
 
 function draw() {
+	//clear();
 
+	image(blurImage,0,0, blurImage.width, blurImage.height);
+
+	imageMode(CENTER);
+
+	if(mouseIsPressed){
+		image(love, mouseX, mouseY, love.width, love.height);
+	} else {
+			image(girl, mouseX, mouseY, girl.width, girl.height);
+	}
 	//STEP 5:
 	//draw your images with image(imagevariable);
 
@@ -35,6 +58,11 @@ function draw() {
 	//You're also going to need an if statement here to check
 	//if the image the user uploaded exists to then draw it
 	//(see image upload example version 2)
+
+	if(img){
+		image(img);
+	}
+
 
 }
 
